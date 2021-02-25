3e0d9358b8d1a1a1c3c646ad8f6c582b6be1e7f8
Killa J <joshuaalexandercross@gmail.com>
2021-02-25 16:19:38 -0600

    multiple rounds complete

--- assets/js/game.js
+++ assets/js/game.js
@@ -21,8 +21,6 @@ var fight = function(enemyName) {
 
     // repeat and execute as long as the enemy-robot is alive
     while(enemyHealth > 0 && playerHealth > 0) {
-    // Alert players that they are starting the round
-  //  window.alert("Welcome to Robot Gladiators!");
 
     // prompt user to FIGHT or SKIP this round
         var promptFight = window.prompt("Would you like to FIGHT or SKIP this battle? Enter 'FIGHT' or 'SKIP' to choose.");
@@ -69,7 +67,7 @@ var fight = function(enemyName) {
 
                 // check player's health
                 if (playerHealth <= 0) {
-                    window.alert(payerName + " has died!");
+                    window.alert(playerName + " has died! You have lost your robot in battle! Game Over");
                     break;
                 } else {
                         window.alert(playerName + " still has " + playerHealth + " health remaining");
@@ -83,6 +81,27 @@ var fight = function(enemyName) {
 };
 
 for (var i = 0; i < enemyNames.length; i++) {
+    if (playerHealth > 0) {
+
+        // let player know what round they are in
+        window.alert("Welcome to Robot Gladiators! Round " + (i + 1));
+
+        // pick new enemy to fight based on the index of the enemyNames array
+        var pickedEnemyName = enemyNames[i];
+
+        // reset enemyHealth before starting new fight
+        enemyHealth = 50
+
+        // use debugger to pause script from running and check what's going on at that moment in the code
+        // debugger;
+
+        // pass the pickedEnemyName variable's value into the fight function, where it will assume the value of the enemyName parameter
+        fight(pickedEnemyName);
+      } 
+    else {
+        window.alert("You have lost your robot in battle! Game Over!");
+        break;
+    }
     var pickedEnemyName = enemyNames[i];
     enemyHealth = 50;
     fight(pickedEnemyName);
